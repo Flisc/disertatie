5552b02f65f6cd35c9d917b294074ea0c4e29208
Adrian Flisc <flisc.adrian@sonrisa.hu>
2023-04-15 12:22:03 +0200

    article-persistence init commit

---
+++ article-persistence/src/main/java/com/pi/articlepersistence/ArticlePersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.pi.articlepersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlePersistenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlePersistenceApplication.class, args);
+    }
+
+}
---
+++ article-persistence/src/test/java/com/pi/articlepersistence/ArticlePersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.articlepersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlePersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
