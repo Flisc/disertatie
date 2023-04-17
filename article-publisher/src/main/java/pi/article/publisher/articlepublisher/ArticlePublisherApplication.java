bffcc89963b1296c3fadd5b868903aa0df3606d1
Adrian Flisc <flisc.adrian@sonrisa.hu>
2023-04-17 21:46:57 +0200

    article-publisher initial commit

---
+++ article-publisher/src/main/java/pi/article/publisher/articlepublisher/ArticlePublisherApplication.java
@@ -0,0 +1,13 @@
+package pi.article.publisher.articlepublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlePublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlePublisherApplication.class, args);
+	}
+
+}
---
+++ article-publisher/src/test/java/pi/article/publisher/articlepublisher/ArticlePublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.article.publisher.articlepublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlePublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
