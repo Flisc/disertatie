44c8c2cd34f5a88399074cb312bddd7e5e23926a
Flisc <fliscadrian@yahoo.com>
2024-06-25 23:23:03 +0300

    demo monolit v2

--- blog/src/main/java/com/example/blog/service/BlogServiceImpl.java
+++ blog/src/main/java/com/example/blog/service/BlogServiceImpl.java
@@ -2,10 +2,13 @@ package com.example.blog.service;
 
 import com.example.blog.faker.SeedService;
 import com.example.blog.model.Article;
+import com.example.blog.model.Notification;
 import com.example.blog.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -23,26 +26,26 @@ public class BlogServiceImpl {
                 .orElseThrow(() -> new Exception("User not found"));
         Article article = articleService.save(SeedService.article(currentUser.getId()));
         String res = "Autorul [" + currentUser.getUserName() + "] a postat un articol nou: " + article.getTitle();
-        ;
-        System.out.println(res);
-//        userService.listUsers().forEach(user -> {
-//            if (user.getSubscribedUsers().contains(currentUser.getId())) {
-//                try {
-//                    notificationService.sendNotification(Notification.builder()
-//                            .message("Catre utilizatorul [" + user.getUserName() + "]\n. " + res)
-//                            .timestamp(LocalDateTime.now())
-//                            .service("Blog service")
-//                            .build());
-//                } catch (Exception e) {
-//                    log.error("Error while sending notification", e);
-//                }
-//            }
-//        });
-//        notificationService.sendNotification(Notification.builder()
-//                .message(String.format("Utilizatorii abonati la [%s] au fost notificati", currentUser.getUserName()))
-//                .timestamp(LocalDateTime.now())
-//                .service("Blog service")
-//                .build());
+//        System.out.println(res);
+        currentUser.getSubscribedUsers().forEach(userSubId -> {
+            String userName = userService.findUserById(userSubId).getUserName();
+            try {
+                notificationService.sendNotification(Notification.builder()
+                        .message("Catre utilizatorul [" + userName + "]\n. " + res)
+                        .timestamp(LocalDateTime.now())
+                        .service("Blog service")
+                        .build());
+            } catch (Exception e) {
+                log.error("Error while sending notification", e);
+            }
+        });
+
+        notificationService.sendNotification(Notification.builder()
+                .message(String.format("Catre [%s] : Utilizatorii abonati au fost notificati. ",
+                        currentUser.getUserName()))
+                .timestamp(LocalDateTime.now())
+                .service("Blog service")
+                .build());
         return res;
     }
 
@@ -50,13 +53,13 @@ public class BlogServiceImpl {
         System.out.println("\n \t -------------------- INREGISTRARE UTILIZATOR NOU --------------------");
         User currentUser = userService.save(SeedService.randomUser());
         String resMessage = "Username[" + currentUser.getUserName() + "] creat cu success in cadrul blogului.";
-        System.out.println(resMessage);
+//        System.out.println(resMessage);
         // TODO: further implementation: activate notifications based on spring property
-//        notificationService.sendNotification(Notification.builder()
-//                .message("Autor nou  ID[" + currentUser.getId() + "], " + resMessage)
-//                .timestamp(LocalDateTime.now())
-//                .service("Blog service")
-//                .build());
+        notificationService.sendNotification(Notification.builder()
+                .message("Autor nou  ID[" + currentUser.getId() + "], " + resMessage)
+                .timestamp(LocalDateTime.now())
+                .service("Blog service")
+                .build());
     }
 
     public String subscribeToAuthor(Long currentUserId, Long subscribedUserId) throws Exception {
@@ -70,12 +73,12 @@ public class BlogServiceImpl {
         author.getSubscribedUsers().add(currentUser.getId());
         userService.save(author);
         String resMessage = String.format("Utilizatorul [" + currentUser.getUserName() + "], s-a abonat la [" + author.getUserName() + "]");
-        System.out.println(resMessage);
-//        notificationService.sendNotification(Notification.builder()
-//                .message(resMessage)
-//                .timestamp(LocalDateTime.now())
-//                .service("Blog service")
-//                .build());
+//        System.out.println(resMessage);
+        notificationService.sendNotification(Notification.builder()
+                .message(resMessage)
+                .timestamp(LocalDateTime.now())
+                .service("Blog service")
+                .build());
         return resMessage;
     }
 
@@ -92,17 +95,17 @@ public class BlogServiceImpl {
         author.getSubscribedUsers().remove(currentUser.getId());
         userService.save(author);
         String message = "Utilizatorul [" + currentUser.getUserName() + "], s-a dezabonat de la [" + author.getUserName() + "]";
-        System.out.println(message);
-//        notificationService.sendNotification(Notification.builder()
-//                .message("Utilizatorul [" + currentUser.getUserName() + "], s-a dezabonat de la [" + author.getUserName() + "]")
-//                .timestamp(LocalDateTime.now())
-//                .service("Blog service")
-//                .build());
-//        notificationService.sendNotification(Notification.builder()
-//                .message("Utilizatorul [" + currentUser.getUserName() + "], s-a dezabonat de la blogul tau.")
-//                .timestamp(LocalDateTime.now())
-//                .service("Blog service")
-//                .build());
+//        System.out.println(message);
+        notificationService.sendNotification(Notification.builder()
+                .message("Utilizatorul [" + currentUser.getUserName() + "], s-a dezabonat de la [" + author.getUserName() + "]")
+                .timestamp(LocalDateTime.now())
+                .service("Blog service")
+                .build());
+        notificationService.sendNotification(Notification.builder()
+                .message("Utilizatorul [" + currentUser.getUserName() + "], s-a dezabonat de la blogul tau.")
+                .timestamp(LocalDateTime.now())
+                .service("Blog service")
+                .build());
     }
 
 
